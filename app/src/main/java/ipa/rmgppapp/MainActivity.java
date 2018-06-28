cc17d778bac2be2f4bf7bf53f8341ad1d088ffed
MultiplexerLab <multiplexer.lab@gmail.com>
2018-06-28 15:41:47 +0600

    Test Commit

--- app/src/main/java/ipa/rmgppapp/MainActivity.java
+++ app/src/main/java/ipa/rmgppapp/MainActivity.java
@@ -1,7 +1,9 @@
 package ipa.rmgppapp;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String deviceId = Settings.Secure.getString(getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+        Log.i("DeviceId", deviceId);
     }
 }
