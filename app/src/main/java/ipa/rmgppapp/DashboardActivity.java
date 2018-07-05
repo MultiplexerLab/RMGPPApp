ea9fbfee98bb23a54681de9c25092052666dab9e
MultiplexerLab <multiplexer.lab@gmail.com>
2018-07-05 16:07:29 +0600

    UI changes

--- app/src/main/java/ipa/rmgppapp/DashboardActivity.java
+++ app/src/main/java/ipa/rmgppapp/DashboardActivity.java
@@ -2,6 +2,9 @@ package ipa.rmgppapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.main_menu:
+                return true;
+            case R.id.save:
+                return true;
+            case R.id.send:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
