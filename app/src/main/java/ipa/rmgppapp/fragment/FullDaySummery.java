74efca0c9f611bc63cbe4b65dfd52daab6213ea8
MultiplexerLab <multiplexer.lab@gmail.com>
2018-09-03 19:24:55 +0600

    Date feature is done

--- app/src/main/java/ipa/rmgppapp/fragment/FullDaySummery.java
+++ app/src/main/java/ipa/rmgppapp/fragment/FullDaySummery.java
@@ -1,13 +1,29 @@
 package ipa.rmgppapp.fragment;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import ipa.rmgppapp.R;
+import ipa.rmgppapp.helper.Endpoints;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class FullDaySummery extends Fragment {
 
@@ -32,5 +48,30 @@ public class FullDaySummery extends Fragment {
     }
 
     private void getSummeryData() {
+
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("supervisor", MODE_PRIVATE);
+        String styleNo = sharedPreferences.getString("styleNo", "");
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, Endpoints.GET_SUMMERY_DATA+"?styleNo="+styleNo, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    JSONObject jsonObject = response.getJSONObject(0);
+                    lineOutputTotal.setText(jsonObject.getString("lineOutput"));
+                    lineTargetTotal.setText(jsonObject.getString("lineTarget"));
+                    activeTarget.setText(jsonObject.getString("activeTarget"));
+                    lineWip.setText(jsonObject.getString("lineWip"));
+                } catch (JSONException e) {
+                    Log.e("SummeryData", e.toString());
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("SummeryData", error.toString());
+            }
+        });
+        queue.add(jsonArrayRequest);
     }
 }
--- app/src/main/java/ipa/rmgppapp/helper/Endpoints.java
+++ app/src/main/java/ipa/rmgppapp/helper/Endpoints.java
@@ -5,7 +5,6 @@ public class Endpoints {
     public static final String CHECK_SUPERVISOR_URL = BASE_URL+"checkSupervisor.php";
     public static final String POST_ASSIGNED_WORKER_URL = BASE_URL+"insertAssignedWorker.php";
     public static final String GET_HR_DATA_URL = BASE_URL+"hrData.php";
-    public static final String GET_WORKER_DATA_URL = BASE_URL+"getAllWorkers.php";
     public static final String GET_PLANNING_DATA_URL = BASE_URL + "planningData.php";
     public static final String GET_PLANNED_LINE_URL = BASE_URL+"getPlannedLines.php";
     public static final String GET_OPERATION_DATA_URL = BASE_URL+"operationData.php";
@@ -14,5 +13,5 @@ public class Endpoints {
     public static final String POST_LINE_TARGET_URL = BASE_URL + "insertLineTarget.php";
     public static final String GET_HOURLY_RECORD_DATA = BASE_URL+"getHourlyData.php";
     public static final String GET_ASSIGNED_WORKER_URL = BASE_URL+"getAssignedWorkerData.php";
-    public static final String GET_DISTINCT_WORKERS_GET = BASE_URL+"getDistinctWorkers.php";
+    public static final String GET_SUMMERY_DATA = BASE_URL+"getSummeryData.php";
 }
